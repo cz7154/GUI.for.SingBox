4a52b66f656162591e40e21eb618322ad33462be
czcz <czcz.com>
2026-06-02 23:12:00 +0800

    feat: 隐藏导航栏

--- frontend/src/components/_common/AppShell.vue
+++ frontend/src/components/_common/AppShell.vue
@@ -5,7 +5,7 @@ import { NavigationBar, TitleBar } from '@/components'
 <template>
   <TitleBar />
   <div class="flex-1 overflow-y-auto flex flex-col p-8">
-    <NavigationBar />
+    <!-- <NavigationBar /> -->
     <div class="flex flex-col overflow-y-auto mt-8 px-8 h-full">
       <RouterView #="{ Component }">
         <KeepAlive>
